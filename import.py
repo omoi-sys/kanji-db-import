fad4462cb0002810482738b844799e3310751f85
omoi-sys <serratoa186@gmail.com>
2021-01-10 01:24:10 +0000

    added numeration to use in random lookup

--- import.py
+++ import.py
@@ -13,6 +13,7 @@ kanjiDB = Db.kanji
 
 @app.route('/insert-kanji/', methods=['GET'])
 def insertOne():
+    num = 1
     for i in kanji:
         res = requests.get('https://kanjiapi.dev/v1/kanji/' + i)
         data = res.json()
@@ -21,9 +22,11 @@ def insertOne():
             'meanings' : data['meanings'],
             'kunyomi' : data['kun_readings'],
             'onyomi' : data['on_readings'],
-            'jlpt' : data['jlpt']
+            'jlpt' : data['jlpt'],
+            'number' : num
         }
         query = kanjiDB.insert_one(kanji_in)
+        num = num + 1
     return 'Kanji inserted.'
 
 if __name__ == '__main__':
